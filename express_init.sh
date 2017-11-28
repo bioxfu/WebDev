2747adea69082392db86954527712b2641a573de
Fu Xing <bio.xfu@gmail.com>
2017-11-28 12:39:17 +0800

    add template which is cloned from chapter07 of getting-MEAN

--- express_init.sh
+++ express_init.sh
@@ -11,11 +11,10 @@ cp -r node_modules/bootstrap/dist public/bootstrap
 cp node_modules/jquery/dist/jquery.min.js public/javascripts
 
 ## clone template
-git clone -b chapter-07 https://github.com/simonholmes/getting-MEAN.git template
-cp -r template/app_api .
-cp -r template/app_server .
-cp template/app.js .
+# git clone -b chapter-07 https://github.com/simonholmes/getting-MEAN.git template
+cp -r WebDev/template/app_api .
+cp -r WebDev/template/app_server .
+cp WebDev/template/app.js .
 rm -r routes views
 
 echo WebDev > .gitignore
-echo template >> .gitignore
