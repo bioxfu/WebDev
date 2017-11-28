8ecb2151b63b33de15dd5e574d50fce76bbe3681
Fu Xing <bio.xfu@gmail.com>
2017-11-28 11:24:12 +0800

    using template from getting-MEAN

--- express_init.sh
+++ express_init.sh
@@ -1,42 +1,21 @@
+## initiate
 express --view=jade
+
+## install modules
 npm install
-
-## Install Bootstrap and add it to the app
+npm install --save mongoose
 npm install --save bootstrap
-cp -r node_modules/bootstrap/dist public/bootstrap
-
-## Install jQuery and add it to the app
 npm install --save jquery
+npm install --save request
+cp -r node_modules/bootstrap/dist public/bootstrap
 cp node_modules/jquery/dist/jquery.min.js public/javascripts
 
-## Changing the folder structure
-mkdir app_server
-mkdir app_server/models
-mkdir app_server/controllers
-mv views app_server
-mv routes app_server
-
-## Using the new views and routes folders
-cat app.js |sed "s/path.join(__dirname, 'views')/path.join(__dirname, 'app_server', 'views')/" \
-| sed "s/\/routes\//\/app_server\/routes\//" > tmp; mv tmp app.js
-
-## Splitting controllers from routes
-echo -e "module.exports.index = function(req, res) {\n  res.render('index', { title: 'Express' });\n};" > app_server/controllers/main.js
-
-cat app_server/routes/index.js | sed 's/function(req, res, next) {/ctrlMain.index);/' \
-| sed "s/res.render('index', { title: 'Express' });//" \
-| sed 's/});//' \
-| sed "s/express.Router();/express.Router();\nvar ctrlMain = require('..\/controllers\/main');/" > tmp
-mv tmp app_server/routes/index.js
-
-## Adding Bootstrap to the template
-cp WebDev/jade/* app_server/views/
-
-## Install Mongoose
-npm install --save mongoose
-cat app.js |sed "s/require('body-parser');/require('body-parser');\nrequire('.\/app_server\/models\/db');/" > tmp; mv tmp app.js
-
-## Adding Mongoose connection file and schema file
-cp WebDev/mongo/* app_server/models/
+## clone template
+git clone -b chapter-07 https://github.com/simonholmes/getting-MEAN.git template
+cp -r template/app_api .
+cp -r template/app_server .
+cp template/app.js .
+rm -r routes views
 
 echo WebDev > .gitignore
+echo template >> .gitignore
